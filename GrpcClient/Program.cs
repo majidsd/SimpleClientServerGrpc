25da505fcf5604ea23f11b84c5829ce30b1efb36
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2021-06-29 09:11:00 +0200

    Adding basic client server app for client issue

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Message"});
+            Console.WriteLine($"Greetings: {reply.Message}");
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
